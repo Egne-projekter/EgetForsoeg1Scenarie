448c7b6162e66f97d7f697de0b401252c93d3b42
Peter Lindberg <pkli62367@edu.ucl.dk>
2024-08-15 12:55:04 +0200

    c

---
+++ EgetForsoeg1Scenarie/EgetForsoeg1Scenarie/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EgetForsoeg1Scenarie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Main.Content = new Forside();
+        }
+
+        private void BtnClickNyFilm(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new NyFilm();
+        }
+
+        private void BtnClickForside(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Forside();
+        }
+    }
+}
---
+++ EgetForsoeg1Scenarie/EgetForsoeg1Scenarie/NyFilm.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EgetForsoeg1Scenarie
+{
+    /// <summary>
+    /// Interaction logic for NyFilm.xaml
+    /// </summary>
+    public partial class NyFilm : Page
+    {
+        private string filepath = @"C:\Users\peter\OneDrive\Desktop\UCL\2. Semester\Programmering og teknologi\The Movies\Eget forsøg 1. Scenarie\filmdata.CSV";
+
+        public NyFilm()
+        {
+            InitializeComponent();
+        }
+
+        private void TilføjFilm_Click(object sender, RoutedEventArgs e)
+        {
+            string titel = tbTitel.Text;
+            int varighed;
+            if (!int.TryParse(tbVarighed.Text, out varighed))
+            {
+                MessageBox.Show("Varighed skal være et tal");
+                return;
+            }
+            string genre = tbGenre.Text;
+
+            string csvLine = $"{titel},{varighed},{genre}";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filepath, true))
+                {
+                    sw.WriteLine(csvLine);
+                }
+
+
+                MessageBox.Show($"Film tilføjet:\nTitel: {titel}\nVarighed: {varighed}\nGenre: {genre}");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fejl under skriving til fil: {ex.Message}");
+            }
+        }
+
+        private void Ryd_Click(object sender, RoutedEventArgs e)
+        {
+            tbTitel.Clear();
+            tbVarighed.Clear();
+            tbGenre.Clear();
+        }
+    }
+}
